340203ce19df00121c0c64d778b4b8eae37dddee
steelec <steelec@gmail.com>
2024-03-10 09:55:54 -0400

    updated init, added small example for simplified approach

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .mvcomp import mysort, feature_gen, norm_covar_inv, mah_dist_feat_mat, mah_dist_mat_2_roi, subject_list, feature_list, compute_average, model_comp, spatial_mvcomp, dist_plot, correlation_fig
+from .mvcomp import model_comp_simplified, compute_average_simplified
 from .version import __version__
 
 # initial hack to not import optional plotting functions if necessary packages do not exist
